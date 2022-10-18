1a3d6a1bc90a10d262903498a8d7e3f7571d4eea
CrazyFenix <114450778+CrazyFenix@users.noreply.github.com>
2022-10-17 20:01:31 -0500

    Trabajos 2 Parcial

---
+++ test/hola.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body background="fondo1.jpg">
+
+    <?php
+    echo '<center> <font face="ink free" size="5" color="black">';
+    echo '<h1>';
+    echo '<p>';
+    echo '<center> Hola a todos</center>';
+    print '<center>Bienvenidos al mundo php</center>';
+    printf('<center>Soy la dianitaaa</center>'); 
+    echo '</h1>';
+    echo '</center>';
+    ?>
+    <center> 
+    <img src="img1.jpg">        
+    </center>
+</body>
+</html>
---
+++ test/hoy.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body background="fondo2.jpg">
+<!--
+Descripción:Tarea numero 2
+Autor:Diana Alejandra Godínez García
+Fecha:12/10/22
+-->
+    <?php
+    echo '<center> <font face="ink free" size="5" color="black">';
+    echo '<h1>';
+    echo '<p>';
+    echo '<center>Diana Alejandra Godínez García</center>';
+    printf( '<center>Programación</center>');
+    print '<center>5PV</center>';
+    print '<center>Desarrolla aplicaciones Web con conexión a base de datos</center>';
+    echo date ('d-m-y');
+    echo '</h1>';
+    echo '</center>';
+    ?>
+    <center> 
+    <img src="img2.jpg">        
+    </center>
+</body>
+</html>
